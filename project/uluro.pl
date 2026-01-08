ba11d884f703ec85979d7ed6944aa6c0d2851b0e
João Castro <up202206575@up.pt>
2026-01-08 17:14:33 +0000

    Etapa 3 : restrições

--- project/uluro.pl
+++ project/uluro.pl
@@ -23,13 +23,34 @@ common_edge(X,Y,Board) :-
     nth1(PositionX,Board,X),
     nth1(PositionY,Board,Y),
     member(PositionX,[1,2]),
-    member(Positiony,[1,2]).
+    member(PositionY,[1,2]).
 common_edge(X,Y,Board) :-
     nth1(PositionX,Board,X),
     nth1(PositionY,Board,Y),
     member(PositionX,[4,5,6]),
-    member(Positiony,[4,5,6]).
+    member(PositionY,[4,5,6]).
 
 position_board(X,L,Board) :-
     nth1(PositionX,Board,X),
     member(PositionX,L).
+
+% Etapa 3 : restrições
+
+anywhere(_,_).
+
+next_to(X,X,_).
+next_to(X,Y,Board) :- consecutive(X,Y,Board).
+next_to(X,Y,Board) :- consecutive(Y,X,Board).
+
+one_space(X,X,_).
+one_space(X,Y,Board) :- space_between(X,Y,Board).
+one_space(X,Y,Board) :- space_between(Y,X,Board).
+
+across(X,X,_).
+across(X,Y,Board) :- across_board(X,Y,Board).
+across(X,Y,Board) :- across_board(Y,X,Board).
+
+same_edge(X,X,_).
+same_edge(X,Y,Board) :- common_edge(X,Y,Board).
+
+position(X,L,Board) :- position_board(X,L,Board).
